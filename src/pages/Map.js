ef40384859b9cea6c40f6f3aaf4c3ab62e2b1eca
Pablo Melendez <pablofer.melendez@gmail.com>
2019-11-23 03:58:00 -0500

    map mock functionality

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -4,8 +4,28 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showRegisterPointModal: false,
       pagina: 'Map',
+      points: [],
     };
+
+    this.showRegisterPointModal = this.showRegisterPointModal.bind(this);
+    this.hideRegisterPointModal = this.hideRegisterPointModal.bind(this);
+  }
+
+  showRegisterPointModal() {
+    this.setState({ showRegisterPointModal: true });
+  }
+
+  hideRegisterPointModal() {
+    this.setState({ showRegisterPointModal: false });
+  }
+
+  handleRegisterPoint(pointInfo) {
+    this.setState((state) => {
+      const { points: oldPoints } = state;
+      return { points: [...oldPoints, pointInfo] };
+    }, this.hideRegisterPointModal);
   }
 
   render() {
