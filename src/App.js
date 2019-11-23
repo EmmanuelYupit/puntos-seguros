7bd8bbcc84b0a75a41b8a34bc00b3d0f1549af27
Pablo Melendez <pablofer.melendez@gmail.com>
2019-11-23 01:37:52 -0500

    end project setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
+import store from './store';
+import theme from './config/theme';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,14 +28,18 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const App = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route path="/home" component={Home} exact />
+            <Route exact path="/" render={() => <Redirect to="/home" />} />
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </Provider>
+    </ThemeProvider>
   </IonApp>
 );
 
